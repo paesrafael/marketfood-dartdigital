d7dc750094843f141d8d7fd9ffa3de87dc65f2b4
Rafael Paes <rfaelpaes@gmail.com>
2020-10-23 10:41:28 -0300

    feat: develops component of listing product

---
+++ src/components/ListingOfProduct/index.js
@@ -0,0 +1,198 @@
+import React, { useState } from 'react'
+import {
+  FaCheckCircle, FaTrashAlt,
+  FaPlusCircle, FaStream,
+} from 'react-icons/fa'
+import { FiCornerDownRight } from 'react-icons/fi'
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
+
+import { useSearchProduct } from '~/contexts'
+
+import SearchProduct from '~/components/SearchProduct'
+import CategoryProduct from '~/components/CategoryProduct'
+
+import {
+  Container,
+  Content,
+  ProductItem,
+  Text,
+  Title,
+  SubTitle,
+  ButtonWrapper,
+  Button,
+  CartContainer,
+  CartContent,
+  CartItem,
+  CartHeader,
+  CartInput,
+  CartFooter,
+  AddedCartContainer,
+  AddedCartItem,
+  Qty,
+} from './styles'
+
+export default function ListingOfProduct() {
+  const [addedCart, setAddedCart] = useState([])
+  const [tabSelected, setTabSelected] = useState(0)
+  const {
+    cart, setCart,
+    searchProduct, searchProductName,
+    productQuantities, setProductQuantities,
+    addedCartProductQuantities, setAddedCartProductQuantities,
+  } = useSearchProduct()
+
+  function handleAddCart(addProduct) {
+    setCart([...cart, addProduct])
+
+    if (tabSelected !== 0) {
+      setTabSelected(0)
+    }
+  }
+
+  function handleRemoveCart(removeProductID) {
+    setCart(cart.filter(cartItems => cartItems.id !== removeProductID))
+  }
+
+  function handleAddedToCart(addedProduct, productItemQuantities) {
+    setAddedCart([...addedCart, addedProduct])
+    setAddedCartProductQuantities([...addedCartProductQuantities, { qtyID: addedProduct.id, qty: productItemQuantities[0].qty }])
+    setCart(cart.filter(removeCartItem => removeCartItem.id !== addedProduct.id))
+  }
+
+  const handleQtyChange = (cartProduct, productItemQuantity) => {
+    const qtyChange = [...productQuantities]
+    qtyChange[cartProduct] = productItemQuantity
+    setProductQuantities([{ qty: productItemQuantity }])
+  }
+
+  return (
+    <Container>
+      <SearchProduct />
+
+      <Content>
+        {searchProduct(searchProductName) &&
+          (searchProduct(searchProductName).length > 0 ? (
+            searchProduct(searchProductName).slice(0, 20).map(product => (
+              <ProductItem
+                key={product.id}
+                type="button"
+                onClick={() => handleAddCart(product)}
+              >
+                <Text className="text">
+                  {product.description}
+                </Text>
+
+                <FaCheckCircle
+                  size="18"
+                  color={`var(--secondary)`}
+                  className="icon-check"
+                />
+              </ProductItem>
+            ))
+          ) : (
+            <Text>loading ...</Text>
+          ))
+        }
+      </Content>
+
+      <Tabs
+        selectedIndex={tabSelected}
+        onSelect={tabSelect => setTabSelected(tabSelect)}
+      >
+        <TabList>
+          <Tab>Minha lista de compras</Tab>
+          <Tab>Meu carrinho</Tab>
+        </TabList>
+
+        <TabPanel>
+          <CartContainer>
+            {!!cart.length >= 1 ? (
+              <CartContent>
+                <Title margin="0 0 10px 0">Sua lista de compras para hoje</Title>
+
+                <ButtonWrapper>
+                  <Button onClick={() => setCart('')}>
+                    <FaStream
+                      size="18"
+                      color={`var(--secondary)`}
+                      className="icon-filled"
+                    />
+
+                    <Text margin="0 0 0 10px" className="text-button">Limpar lista</Text>
+                  </Button>
+                </ButtonWrapper>
+
+                {cart.map(cartItem => (
+                  <CartItem key={cartItem.id}>
+                    <CartHeader>
+                      <Text className="text">{cartItem.description}</Text>
+
+                      <CartInput
+                        type="number"
+                        name={`product-${cartItem.id}`}
+                        min="1"
+                        onChange={(qty) => (handleQtyChange(`product-${cartItem.id}`, qty.target.value))}
+                      />
+                    </CartHeader>
+
+                    <CartFooter>
+                      <FaTrashAlt
+                        size="18"
+                        color={`var(--red)`}
+                        className="icon"
+                        onClick={() => handleRemoveCart(cartItem.id)}
+                      />
+
+                      <FaPlusCircle
+                        size="18"
+                        color={`var(--secondary)`}
+                        className="icon-filled"
+                        onClick={() => handleAddedToCart(cartItem, productQuantities)}
+                      />
+                    </CartFooter>
+                  </CartItem>
+                ))}
+              </CartContent>
+            ) : (
+              <Text>Nenhum produto na sua lista de compras!</Text>
+            )}
+          </CartContainer>
+        </TabPanel>
+
+        <TabPanel>
+          {!!addedCart.length >= 1 ? (
+            <AddedCartContainer>
+              <Title className="red">Produtos em seu carrinho</Title>
+
+              {addedCart.map(addedCart => (
+                <AddedCartItem key={`addedProduct-${addedCart.id}`}>
+                  <SubTitle>
+                    {addedCart.description}
+                  </SubTitle>
+
+                  {addedCartProductQuantities.length >= 1 && (
+                    addedCartProductQuantities.filter((searchQty) => searchQty.qtyID === addedCart.id).map(quantity => (
+                      <Qty key={`qty-${quantity.id}`}>
+                        <FiCornerDownRight
+                          size="18"
+                          color={`var(--secondary)`}
+                        />
+                        Qtd: {quantity.qty}
+                      </Qty>
+                    ))
+                  )}
+
+                  <CategoryProduct categoryID={addedCart.category_id} />
+                </AddedCartItem>
+              ))}
+            </AddedCartContainer>
+          ) : (
+            <Text>Você ainda não pegou nenhum produto no mercado!</Text>
+          )}
+        </TabPanel>
+      </Tabs>
+    </Container>
+  )
+}
---
+++ src/components/ListingOfProduct/styles.js
@@ -0,0 +1,184 @@
+import styled, { css } from 'styled-components'
+
+const Card = css`
+  padding: 10px;
+  margin: 0 10px 20px 10px;
+  border: 1px solid var(--secondary);
+  border-radius: 10px;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+const Container = styled.section`
+  margin: 0 auto;
+  width: 80%;
+
+  @media (min-width: 1301px) {
+    width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+
+const Content = styled.div`
+  width: 100%;
+  display: inline-block;
+
+  .icon-check {
+    margin-top: 10px;
+  }
+
+  @media (max-width: 1130px) {
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: left;
+    overflow-y: auto;
+  }
+`
+
+const ProductItem = styled.button`
+  ${Card}
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--primary);
+
+    .text {
+      color: var(--white);
+    }
+
+    .icon-check {
+      color: var(--white) !important;
+    }
+  }
+
+  .text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .icon-check {
+    margin-left: 5px;
+  }
+`
+
+const Text = styled.p`
+  margin: ${(props) => (props.margin ? props.margin : '0')};
+  color: var(--text);
+  font-size: ${(props) => (props.size ? props.size : '16px')};
+`
+
+const Title = styled.h2`
+  margin: ${(props) => (props.margin ? props.margin : '0 0 20px 0')};
+  font-size: 20px;
+
+  &.red {
+    color: var(--red);
+  }
+`
+
+const SubTitle = styled.h3``
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+`
+
+const Button = styled.button`
+  padding: 10px;
+  border: 1px solid var(--secondary);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    border-color: var(--primary);
+    background-color: var(--primary);
+
+    .text-button {
+      color: var(--white);
+    }
+  }
+`
+
+const CartContainer = styled.div`
+  display: block;
+`
+
+const CartContent = styled.div``
+
+const CartItem = styled.div`
+  ${Card}
+
+  @media (max-width: 768px) {
+    margin: 0 auto;
+    margin-bottom: 30px;
+    width: 85%;
+  }
+`
+
+const CartHeader = styled.div``
+
+const CartInput = styled.input`
+  padding: 5px;
+  margin-top: 10px;
+  text-align: center;
+  border: 1px solid var(--secondary);
+  border-radius: 10px;
+`
+
+const CartFooter = styled.footer`
+  padding-top: 10px;
+  margin-top: 10px;
+  border-top: 1px solid var(--secondary);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .icon {
+    cursor: pointer;
+
+    &:hover {
+      color: var(--secondary) !important;
+    }
+  }
+
+  .icon-filled {
+    cursor: pointer;
+
+    &:hover {
+      fill: var(--primary) !important;
+    }
+  }
+`
+
+const AddedCartContainer = styled.div``
+
+const AddedCartItem = styled.div`
+  ${Card}
+
+  @media (max-width: 768px) {
+    margin: 0 auto;
+    margin-bottom: 30px;
+    width: 85%;
+  }
+`
+
+const Qty = styled.span``
+
+export {
+  Container, Content,
+  ProductItem, Text, Title, SubTitle,
+  ButtonWrapper, Button,
+  CartContainer, CartContent, CartItem,
+  CartHeader, CartInput, CartFooter,
+  AddedCartContainer, AddedCartItem, Qty,
+}
